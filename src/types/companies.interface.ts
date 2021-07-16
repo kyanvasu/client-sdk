75287f5d9d5e3035783d4ffc11c0442a096e505b
Rogelio Morey <sparohawk@gmail.com>
2021-07-15 22:00:17 -0400

    feat: add companies interface

---
+++ src/types/companies.interface.ts
@@ -0,0 +1,12 @@
+export default interface Companies {
+  id?: number;
+  name: string;
+  address?: string;
+  zipcode?: string;
+  language?: string;
+  timezone?: string;
+  phone?: string;
+  users_id?: number;
+  created_at?: string;
+  updated_at?: string;
+}
